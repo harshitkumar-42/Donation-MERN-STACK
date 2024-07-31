4b8049f520ec5fd6addf3ce0900578c7491ea847
HarshitKumar9721 <14harshitkumar@gmail.com>
2024-07-31 15:19:10 +0530

    commit

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../pages/Hero'
+import Members from '../pages/Members'
+import Qualities from '../pages/Qualities'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+      <Members/>
+      <Qualities/>
+    </div>
+  )
+}
+
+export default Home
